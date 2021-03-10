386d311f0935f277b8adf2b8e07aaffe3e4bfbf7
unknown <didwjddn2019@gmail.com>
2021-03-10 21:41:39 +0900

    make post routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,37 @@
 import express from 'express'
+import hpp from 'hpp';
+import mongoose, { mongo } from 'mongoose'
+import config from './config';
+import cors from "cors";
+import helmet from "helmet"
+
+//Routes
+import postsRoutes from './routes/api/post';
+import morgan from 'morgan';
 
 const app = express();
+const {MONGO_URI} = config;
 
+app.use(helmet());
+app.use(hpp());
+
+app.use(cors({origin: true, cerdentials: true}));
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+
+mongoose
+.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,  
+})
+.then(()=> console.log("mongoDB connecting Success! "))
+.catch((e)=> console.log(e))
+
+//Use routes
 app.get('/');
+app.use('/api/post', postsRoutes)
 
 export default app;
