96c542d03fd14cd89c492887cbec3eeef558d791
unknown <didwjddn2019@gmail.com>
2021-03-16 22:21:29 +0900

    navbar

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-
+import AppNavbar from "../components/AppNavbar"
 const MyRouter = (params) => {
     return (
         < Fragment >
+            <AppNavbar/>
             <Header/>
-                <h1>hello my body</h1>
+                <h1>hello Body</h1>
             <Footer />
         </Fragment >
     )
